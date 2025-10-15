3e1642844f4ff3fb274b0eab410b156884e2f037
chumachenkoUA <kirito22122005@gmail.com>
2025-10-15 19:59:52 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,33 @@
+"use strict";
+const table = document.querySelector("table");
+const toNumber = (value)=>{
+    let cleaned = "";
+    for (const char of value)if ("0123456789".includes(char)) cleaned += char;
+    if (cleaned === "") return NaN;
+    return Number(cleaned);
+};
+table.addEventListener("click", (e)=>{
+    const th = e.target.closest("th");
+    if (!th) return;
+    const columnIndex = th.cellIndex;
+    const tBodies = [
+        ...table.tBodies
+    ];
+    if (!tBodies) return;
+    tBodies.forEach((tBody)=>{
+        const rows = [
+            ...tBody.rows
+        ];
+        rows.sort((rowA, rowB)=>{
+            const a = rowA.cells[columnIndex].textContent.trim();
+            const b = rowB.cells[columnIndex].textContent.trim();
+            const numberA = toNumber(a);
+            const numberB = toNumber(b);
+            if (!isNaN(numberA) && !isNaN(numberB)) return numberA - numberB;
+            return a.localeCompare(b);
+        });
+        tBody.append(...rows);
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
